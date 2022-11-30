e19d6bb1ab4ec4d14e5910b2315294ad38b2e7c4
Lucas Dallamico <lucas.dallamico@hotmail.com>
2022-11-29 23:10:24 -0300

    Gigantesca alteração V2

--- include/settings_ht.h
+++ include/settings_ht.h
@@ -17,9 +17,10 @@
 typedef enum
 {
     INITIAL = 1,
-    CONTINUITY,
-    DUCT_CYCLE,
-    SENOIDAL,
+    CONTINUIDADE_CABO_1,
+    CONTINUIDADE_CABO_2,
+    CONTINUIDADE_CABO_3,
+    CONTINUIDADE_CABO_4,
 } ROUTINE_TEST;
 
 typedef enum 
@@ -41,6 +42,8 @@ typedef enum
     TEST_2_FAIL,
     TEST_3_PASS,
     TEST_3_FAIL,
+    TEST_4_PASS,
+    TEST_4_FAIL,
 } FEEDBACK_TEST;
 
 typedef enum 
@@ -66,19 +69,23 @@ typedef enum
 #if defined(CAIXA1)
     #define LED_RED     13
     #define LED_GRE     12
-    #define PIN_12V     14
-    #define PIN_5V      27
-    #define PIN_CAN     26
+    #define PIN_CABO_1  14
+    #define PIN_CABO_2  27
+    #define PIN_CABO_3  26
+    #define PIN_CABO_4  32 
     #define BUTTON_1    25
     #define BUTTON_2    33
-    #define BUTTON_3    32
 #elif defined(CAIXA2)
-    #define PIN_12V     14
-    #define PIN_5V      27
-    #define PIN_CAN     26
-    #define RANGE_HIGH_SIGNAL_LEVEL 3700
-    #define DEFAULT_DUCT_CYCLE (1/30)*1000000
+    #define PIN_CABO_1  14
+    #define PIN_CABO_2  27
+    #define PIN_CABO_3  26
+    #define PIN_CABO_4  32 
 #endif
+
+#define TIME_FEEDBACK_TEST  200
+#define DELAY_LOOP          400
+
+
 
 typedef struct {
     EVENT_SYSTEM _event;
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,11 @@
 #include "../include/settings_ht.h"
 
-bool negador = 0;
-
 // REPLACE WITH THE MAC Address of your receiver 
 uint8_t broadcastAddress[6] = {0xC0, 0x49, 0xEF, 0xD1, 0x9C, 0xD4};
-
-const uint64_t period_duct_cycle = (1/60)*1000000 /* ms */;
-bool semaphro_dc = 0;
-uint32_t Pulse_Conter = 0;
-uint16_t Period_Read_Duct_Cycle = 0;
-hw_timer_t *My_timer = NULL; /*Usar para interrup timer*/
+uint8_t const Sequencia_Cabo[4] = {PIN_CABO_1, PIN_CABO_2, PIN_CABO_3, PIN_CABO_4};
 
 FEEDBACK_TEST Read_Current_Test(ROUTINE_TEST routine_current);
-
-
-void IRAM_ATTR Interrupt_GPIO()
-{
-  Pulse_Conter++;
-}
-
-void Timer__CalcDuctCycle(void)
-{
-  Period_Read_Duct_Cycle = (uint16_t)(period_duct_cycle / Pulse_Conter);
-}
+FEEDBACK_TEST Converte_Feedback(bool isPass, ROUTINE_TEST routine_current);
 
 DeviceBox caixa_2;
 
@@ -30,28 +13,15 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println();
-  #if defined(CAIXA1)
-    pinMode(LED_RED, OUTPUT);
-    pinMode(LED_GRE, OUTPUT);
-    pinMode(PIN_12V, OUTPUT);
-    pinMode(PIN_5V , OUTPUT);
-    pinMode(PIN_CAN, OUTPUT);
-    pinMode(BUTTON_1, INPUT_PULLDOWN);
-    pinMode(BUTTON_2, INPUT_PULLDOWN);
-    pinMode(BUTTON_3, INPUT_PULLDOWN);
+  #if defined(CAIXA2)
     uint8_t i;
-    for (i = 0; i < sizeof(Pinout) - 1; i++)
+    for (i = 0; i < 4; i++)
     {
-      digitalWrite(Pinout[i], LOW);
+      pinMode(Sequencia_Cabo[i], INPUT_PULLDOWN);
     }
-  #elif defined(CAIXA2)
-    pinMode(PIN_12V, INPUT_PULLDOWN);
-    pinMode(PIN_5V , INPUT_PULLDOWN);
-    pinMode(PIN_CAN, INPUT_PULLDOWN);
   #endif
-  attachInterrupt(PIN_12V, Interrupt_GPIO, FALLING);
   caixa_2.Initializer(broadcastAddress);
-  Serial.println("Setup");
+  Serial.println("INICIOU");
 }
 
 void loop()
@@ -65,56 +35,70 @@ void loop()
   caixa_2.Debug_SeeVariables();
   caixa_2.setFeedbacTest(Read_Current_Test(caixa_2.getCurrentRoutine()));
   caixa_2.Process();
-  delay(500);
+  delay(DELAY_LOOP);
 }
 
 FEEDBACK_TEST Read_Current_Test(ROUTINE_TEST routine_current)
 {
-  switch (routine_current)
+  if (routine_current != INITIAL)
   {
-    case INITIAL:
-      ;
-      /* DO Nothing */
-      break;
-    case CONTINUITY:
-      if (semaphro_dc)
+    uint8_t index = 0;
+    for (index = 0; index < 4; index++)
+    {
+      if (routine_current == Sequencia_Cabo[index])
       {
-        timerAlarmDisable(My_timer);
-        semaphro_dc = 0;
-      }
-      if ((analogRead(PIN_12V) > RANGE_HIGH_SIGNAL_LEVEL) &&
-          (analogRead(PIN_5V) > RANGE_HIGH_SIGNAL_LEVEL) &&
-          (analogRead(PIN_CAN) > RANGE_HIGH_SIGNAL_LEVEL))
+        if(digitalRead(Sequencia_Cabo[index]) == LOW)
         {
-          return TEST_1_PASS;
+          return Converte_Feedback(false, routine_current);
         }
-        else
-        {
-          return TEST_1_FAIL;
-        }
-      break;
-    case DUCT_CYCLE:
-      /* Rotina para a aquição do DUCT_CYCLE */
-      if (!semaphro_dc)
-      {
-        My_timer = timerBegin(0, 80, true);
-        timerAttachInterrupt(My_timer, &Timer__CalcDuctCycle, true);
-        timerAlarmWrite(My_timer, DEFAULT_DUCT_CYCLE/2, true);
-        timerAlarmEnable(My_timer); //Just Enable
-        semaphro_dc = 1;
-      }
-      if ((int)abs((DEFAULT_DUCT_CYCLE-Period_Read_Duct_Cycle)/Period_Read_Duct_Cycle)*100 < 5 &&
-          Period_Read_Duct_Cycle != 0)
-      {
-        return TEST_2_PASS;
       }
       else
       {
-        return TEST_2_PASS;
+        if(digitalRead(Sequencia_Cabo[index]) == HIGH)
+        {
+          return Converte_Feedback(false, routine_current);
+        }
       }
+    }
+    return Converte_Feedback(true, routine_current);
+  }
+  return Converte_Feedback(false, routine_current);
+}
+
+FEEDBACK_TEST Converte_Feedback(bool isPass, ROUTINE_TEST routine_current)
+{
+  switch (routine_current)
+  {
+    // -----------------------
+    case CONTINUIDADE_CABO_1:
+      if (isPass)
+        return TEST_1_PASS;
+      else
+        return TEST_1_FAIL;
       break;
-  default:
-    break;
+    // -----------------------
+    case CONTINUIDADE_CABO_2:
+      if (isPass)
+        return TEST_2_PASS;
+      else
+        return TEST_2_FAIL;
+      break;
+    // -----------------------
+    case CONTINUIDADE_CABO_3:
+      if (isPass)
+        return TEST_3_PASS;
+      else
+        return TEST_3_FAIL;
+      break;
+    // -----------------------    
+    case CONTINUIDADE_CABO_4:
+      if (isPass)
+        return TEST_4_PASS;
+      else
+        return TEST_4_FAIL;
+      break;    
+    default:
+      break;
   }
   return ANY_TEST_RUNNING;
 }
